e3d7cdbc12b30fb00cb2ee000df26a0d5182be83
wt051453 <wt051453@gmail.com>
2019-01-31 14:26:33 -0800

    Update to live project

---
+++ BundleConfig/BundleConfig.cs
@@ -0,0 +1,8 @@
+﻿
+// Copy the showCalendar() method from employeeindex.js into a newly created script file calendar.js and added it to the BundleConfig.cs
+
+bundles.Add(new ScriptBundle("~/bundles/fullcalendar").Include(
+					  "~/Scripts/moment.min.js",
+					  "~/Scripts/fullcalendar/fullcalendar.min.js",
+					  "~/Scripts/calendar.js"));
+
